69d4d1c11ca090dddcaec286e1fe215683af1aa6
Egor Antonov <me@eantonov.name>
2018-06-16 14:47:43 +0300

    fetch feed mock

--- backend.py
+++ backend.py
@@ -3,22 +3,39 @@ from cherrypy.lib.static import serve_file
 import os
 from argparse import ArgumentParser
 
+
 def static_path(path):
     return os.path.abspath(os.path.join('static', path))
 
 
 class Viewer(object):
-
     @cherrypy.expose
     def index(self):
         raise cherrypy.HTTPRedirect('/feed')
 
+    @cherrypy.expose
     def game(self):
         serve_file(static_path('game.html'))
 
     @cherrypy.expose
     def feed(self):
         serve_file(static_path('feed.html'))
+
+    @cherrypy.expose(['fetch-feed'])
+    @cherrypy.tools.json_out()
+    def fetch_feed(self, count=10):
+        return [{
+            'items': [{
+                'link': 'https://ru.aliexpress.com/item/-/32825303424.html',
+                'image': 'https://ae01.alicdn.com/kf/HTB1THlsfazB9uJjSZFMq6xq4XXaf/Bobokateer-Vogue.jpg_220x220.jpg'
+            }, {
+                'link': 'https://ru.aliexpress.com/item/-/32852682510.html',
+                'image': 'https://ae01.alicdn.com/kf/HTB18MEDXbSYBuNjSspiq6xNzpXar/-.jpg_220x220.jpg'
+            }],
+            'votes': 100,
+            'rating': 55,
+            'author': '@elderos'
+        }] * count
 
 
 def main(args):
@@ -34,6 +51,7 @@ def main(args):
     viewer = Viewer()
 
     cherrypy.quickstart(viewer, '', config)
+
 
 if __name__ == '__main__':
     parser = ArgumentParser()
