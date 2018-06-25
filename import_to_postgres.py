8578c97af54ac6788d9699c553d06802bb41ba04
Egor Antonov <me@eantonov.name>
2018-06-25 23:23:45 +0300

    faster import

--- import_to_postgres.py
+++ import_to_postgres.py
@@ -3,6 +3,58 @@ import csv
 import os
 import ujson as json
 from common import connect
+from multiprocessing.dummy import Pool, Lock
+
+
+counter = 0
+locker = Lock()
+
+
+def item_from_row(row, itemno):
+    img = row['INPUT:item%s_image' % itemno]
+    basename, ext = os.path.splitext(img)
+    pattern = 'https://ae01.alicdn.com/kf/%(id)s/-%(ext)s_%(size)s%(ext)s'
+    small_img = pattern % {
+        'id': basename,
+        'ext': ext,
+        'size': '220x220'
+    }
+    big_img = pattern % {
+        'id': basename,
+        'ext': ext,
+        'size': '640x640'
+    }
+    return {
+        'item_id': row['INPUT:item%s_id' % itemno],
+        'link': 'https://aliexpress.com/item/-/%s.html' % row['INPUT:item%s_id' % itemno],
+        'image_small': small_img,
+        'image_big': big_img,
+    }
+
+
+def insert(row):
+    global counter
+    conn = connect()
+    conn.autocommit = True
+    cursor = conn.cursor()
+    jdata = json.loads(row['HINT:text'])
+    data = {
+        'items': [
+            item_from_row(row, 1),
+            item_from_row(row, 2),
+        ],
+        'likes': jdata['likes'],
+        'votes': jdata['votes']
+    }
+    cursor.execute('insert into pairs (data) values (%(jdata)s)', {
+        'jdata': json.dumps(data)
+    })
+    cursor.close()
+    conn.close()
+    with locker:
+        counter += 1
+        if (counter + 1) % 10000 == 0:
+            print '%s items done' % (counter + 1)
 
 
 def main(args):
@@ -13,42 +65,10 @@ def main(args):
         cursor.execute('truncate table pairs;')
     with open(args.file, 'r') as f:
         reader = csv.DictReader(f, delimiter='\t')
-        for i, row in enumerate(reader):
-            def item_from_row(row, itemno):
-                img = row['INPUT:item%s_image' % itemno]
-                basename, ext = os.path.splitext(img)
-                pattern = 'https://ae01.alicdn.com/kf/%(id)s/-%(ext)s_%(size)s%(ext)s'
-                small_img = pattern % {
-                    'id': basename,
-                    'ext': ext,
-                    'size': '220x220'
-                }
-                big_img = pattern % {
-                    'id': basename,
-                    'ext': ext,
-                    'size': '640x640'
-                }
-                return {
-                    'item_id': row['INPUT:item%s_id' % itemno],
-                    'link': 'https://aliexpress.com/item/-/%s.html' % row['INPUT:item%s_id' % itemno],
-                    'image_small': small_img,
-                    'image_big': big_img,
-                }
-
-            jdata = json.loads(row['HINT:text'])
-            data = {
-                'items': [
-                    item_from_row(row, 1),
-                    item_from_row(row, 2),
-                ],
-                'likes': jdata['likes'],
-                'votes': jdata['votes']
-            }
-            cursor.execute('insert into pairs (data) values (%(jdata)s)', {
-                'jdata': json.dumps(data)
-            })
-            if i % 10000 == 9999:
-                print('%s items done' % (i + 1))
+        pool = Pool(50)
+        pool.map(insert, reader)
+        # for row in reader:
+        #     insert(cursor, row)
     cursor.close()
 
 
