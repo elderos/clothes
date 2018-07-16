ada8b301fc019f651145e3c0230cc9a98509cf95
volerog <volerog@gmail.com>
2018-07-17 03:47:52 +0500

    remove gen data

--- model_gen_data.py
+++ model_gen_data.py
@@ -1,7 +1,1 @@
 
-
-USERNAME_1 = ['Kilo', 'Mega', 'Nedo', 'Mini', 'Super', 'Pere', 'Cherez', 'Uber']
-USERNAME_2 = ['Bot', 'Ban', 'Fon', 'Line', 'Ball', 'Bell', 'Arc', 'Rack', 'Tooth', 'Goal']
-
-NAME = ['Anna', 'Maria', 'Mary', 'Vasya', 'John', 'Sam', 'Li', 'July', 'Peter']
-SURNAME = ['Peters', 'Smith', 'Hopkins', 'Si', 'Poppins', 'Pupkin']
